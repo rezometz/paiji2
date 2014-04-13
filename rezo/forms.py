702e9b24f85926d972369247687c7b4589465864
Gabriel Pichot <gabriel.pichot@gmail.com>
2014-04-13 03:21:21 +0200

    clean_username method is now independant of the model

--- rezo/forms.py
+++ rezo/forms.py
@@ -7,6 +7,19 @@ class ConfirmForm(forms.Form):
     pass
 
 class UserCreationForm(auth.forms.UserCreationForm):
+    def clean_username(self):
+        # Since User.username is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM. See #13147.
+        username = self.cleaned_data["username"]
+        try:
+            self.Meta.model._default_manager.get(username=username)
+        except self.Meta.model.DoesNotExist:
+            return username
+        raise forms.ValidationError(
+            self.error_messages['duplicate_username'],
+            code='duplicate_username',
+        )
+
     class Meta:
         model = auth.get_user_model()
         fields = ()
