22c8231090379540c309f997833703f897e06773
Gabriel Pichot <gabriel.pichot@gmail.com>
2014-04-13 02:34:34 +0200

    Request is now passed in the context of the inclusion tag and the form is bootstraped

--- cov/templatetags/cov_tag.py
+++ cov/templatetags/cov_tag.py
@@ -4,8 +4,11 @@ from django.utils import timezone
 
 register = template.Library()
 
-@register.inclusion_tag('cov/cov_block.html')
-def get_cov():
+@register.inclusion_tag('cov/cov_block.html', takes_context=True)
+def get_cov(context):
 	list_cov = models.Covoiturage.objects.all().filter(good_until__gte=timezone.now()).order_by('good_until')[:5]
-	ctx_data = {'cov' : list_cov}
+	ctx_data = {
+        'cov' : list_cov,
+        'request': context['request'],
+    }
 	return ctx_data
