e09aee5a9cf6c1f42a31177f548045d7125767f2
Gabriel Pichot <gabriel.pichot@gmail.com>
2014-04-13 03:16:50 +0200

    Deleted direct reference to User in favour of get_user_model()

--- rezo/forms.py
+++ rezo/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 from django.contrib import auth
 
-from .models import AccountRecovery, User
+from .models import AccountRecovery
 
 class ConfirmForm(forms.Form):
     pass
 
 class UserCreationForm(auth.forms.UserCreationForm):
     class Meta:
-        model = User
+        model = auth.get_user_model()
         fields = ()
 
 class UserAuthenticationForm(auth.forms.AuthenticationForm):
