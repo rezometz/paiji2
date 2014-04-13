1d0a62a76d951d65e4e35c4d55b82747b4261d40
liusiqi43 <me@siqi.fr>
2014-04-13 23:03:12 +0200

    readmore for bullentin + cov fonctionnel

--- cov/models.py
+++ cov/models.py
@@ -1,21 +1,19 @@
 # -*- coding: UTF-8 -*-
 
 from django.db import models
-from django.core.validators import *
-from django.forms import ModelForm
+# from django.core.validators import *
 from django.utils import timezone
 from datetime import timedelta
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Covoiturage(models.Model):
 	"""model for Covoiturage"""
 
-	# TODO poster and poster_email should be replaced by a FK to paiji user
-	poster = models.CharField(
-		max_length=50,
-		blank=False,)
-	poster_email = models.EmailField(
-		blank=False,)
+	poster = models.ForeignKey(
+        get_user_model(),
+        related_name='covs',
+    )
 
 	ANNONCE_TYPE = (
         (0, 'Propose'),
@@ -23,59 +21,28 @@ class Covoiturage(models.Model):
     )
 	annonce_type = models.IntegerField(choices=ANNONCE_TYPE, blank=False)
 
-	# itinerary = models.CharField(
-	# 	max_length=50,
-	# 	help_text="Please specify your itinerary as Metz-Paris-Compiegne (without accent) etc...",
-	# 	validators=[
- #            RegexValidator(
- #                r'^(([^\W\d]|[u+0100-u+0127])+-([^\W\d]|[u+0100-u+0127])+)+$',
- #                'Please specify your itinerary as Metz-Paris-Compiegne(without accent) etc...',
- #                'Invalid itinerary'
- #            ),
- #            MinLengthValidator(3),
- #        ],)
-
-	# n_places = models.IntegerField(
-	# 	validators=[
-	# 		MinValueValidator(1),
-	# 		MaxValueValidator(50),
-	# 	],)
-
 	good_until = models.DateTimeField(
-		'offer will be canceled on this date',
+		'Annonce reste valable jusqu`à la date ci-dessous',
 		default = timezone.now()+timedelta(days=3))
 
-	# price_per_trip = models.IntegerField(
-	# 	blank = True,
-	# 	null = True,
-	# 	help_text = 'leave blank for price on request',
-	# 	validators=[
-	# 		MinValueValidator(1),
-	# 	],)
+	posted_at = models.DateTimeField(
+        'Posted at',
+    )
 
 	notes = models.CharField(
 		max_length=150)
 
 	def __unicode__(self):
-		rep = self.poster
+		rep = self.poster.first_name + self.poster.last_name
 		rep += ' propose ' if self.ANNONCE_TYPE == 0 else ' cherche '
 		rep += self.notes
 		return rep
 
-	def future(self):
-		return timezone.now() < self.good_until
+	def save(self, *args, **kwargs):
+		if self.pk is None:
+			self.posted_at = timezone.now()
 
-class CovoiturageForm(ModelForm):
+		super(Covoiturage, self).save(*args, **kwargs)
+
 	class Meta:
-		model = Covoiturage
-		fields = [
-		'poster',
-		'poster_email',
-		'annonce_type',
-		'good_until',
-		# 'itinerary',
-		# 'dept_datetime',
-		# 'ret_datetime',
-		# 'n_places',
-		# 'price_per_trip',
-		'notes']
+		ordering = ('-posted_at', )
--- cov/templatetags/cov_tag.py
+++ cov/templatetags/cov_tag.py
@@ -1,12 +1,13 @@
 from django import template
-from cov import models
+from ..models import Covoiturage
 from django.utils import timezone
 
 register = template.Library()
 
 @register.inclusion_tag('cov/cov_block.html', takes_context=True)
-def get_cov(context):
-	list_cov = models.Covoiturage.objects.all().filter(good_until__gte=timezone.now()).order_by('good_until')[:5]
+def get_cov(context, nb):
+	list_cov = Covoiturage.objects.select_related('poster').all().filter(good_until__gte=timezone.now()).order_by('good_until')[:nb]
+	print list_cov
 	ctx_data = {
         'cov' : list_cov,
         'request': context['request'],
