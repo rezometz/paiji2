727f1de69a3f2211d97a2c5ed412eacb90d11619
Gabriel Pichot <gabriel.pichot@gmail.com>
2014-04-20 14:52:26 +0200

    Fix : cov list is fetch in one queries (before it was fetched twice)

--- cov/templatetags/cov_tag.py
+++ cov/templatetags/cov_tag.py
@@ -6,10 +6,9 @@ register = template.Library()
 
 @register.inclusion_tag('cov/cov_block.html', takes_context=True)
 def get_cov(context, nb):
-	list_cov = Covoiturage.objects.select_related('poster').all().filter(good_until__gte=timezone.now()).order_by('good_until')[:nb]
-	print list_cov
-	ctx_data = {
-        'cov' : list_cov,
+	return {
+        'cov' : Covoiturage.objects.select_related('poster').filter(
+            good_until__gte=timezone.now()
+        ).order_by('good_until')[:nb],
         'request': context['request'],
     }
-	return ctx_data
