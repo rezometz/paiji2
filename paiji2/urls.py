1a20788beae639dd31c3fb6e2d562604c8d8c49e
liusiqi43 <me@siqi.fr>
2014-06-18 19:05:14 +0200

    fix #12

--- paiji2/urls.py
+++ paiji2/urls.py
@@ -25,6 +25,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     (r'^tinymce/', include('tinymce.urls')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
 
 urlpatterns += modules.get_patterns()
