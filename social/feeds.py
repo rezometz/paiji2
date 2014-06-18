ff61805e2bf973cd2db5ab191000a2ba805ccbdf
Gabriel Pichot <gabriel.pichot@gmail.com>
2014-06-18 18:58:06 +0200

    Added RSS feed to social

---
+++ social/feeds.py
@@ -0,0 +1,27 @@
+from django.contrib.syndication.views import Feed
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
+
+from .models import Message
+
+
+class LatestEntriesFeed(Feed):
+    title = _("Social Latest Entries")
+    link = "/feeds/"
+    description = _("Latest entries from all the workgroups")
+
+    def items(self):
+        return Message.objects.order_by('-pubDate')[:10]
+
+    def item_title(self, item):
+        return '[{group}] {title}'.format(
+            group=item.group,
+            title=item.title,
+        )
+
+    def item_description(self, item):
+        return item.content
+
+    def item_link(self, item):
+        return reverse('index')
--- social/urls.py
+++ social/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
 from .views import MessageCreateView, MessageListView
+from .feeds import LatestEntriesFeed
 
 urlpatterns = patterns('',
     url(
@@ -9,4 +10,12 @@ urlpatterns = patterns('',
         login_required(MessageCreateView.as_view()),
         name="newsfeed-add",
     ),
+
+    # Feeds
+    url(
+        r'^feeds/latest$',
+        LatestEntriesFeed(),
+        name="feed-latest",
+    ),
+
 )
