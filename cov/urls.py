5c5c8b3c59cf2e9f3aafc601f268821087859da0
liusiqi43 <me@siqi.fr>
2014-06-21 18:17:37 +0200

    mute out past covs and add edit/remove functionalites

--- cov/urls.py
+++ cov/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from .views import CovListView, CovCreateView
+from .views import *
 
 urlpatterns = patterns('',
     url(
@@ -19,5 +19,14 @@ urlpatterns = patterns('',
         login_required(CovCreateView.as_view()),
         name="cov-add",
     ),
-
+    url(
+        r'edit/(?P<pk>[0-9]+)/$',
+        login_required(CovEditView.as_view()),
+        name="cov-edit",
+    ),
+    url(
+        r'delete/(?P<pk>[0-9]+)/$',
+        login_required(CovDeleteView.as_view()),
+        name="cov-delete",
+    ),
 )
