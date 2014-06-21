ef83abf116203507d666539b2aa059c5600e7635
liusiqi43 <me@siqi.fr>
2014-06-21 18:24:44 +0200

    import from django.http import HttpResponseNotFound

--- bulletin_board/views.py
+++ bulletin_board/views.py
@@ -1,6 +1,6 @@
 from django.views import generic
 from django.core.urlresolvers import reverse
-
+from django.http import HttpResponseNotFound
 from django.utils.translation import ugettext_lazy as _
 
 from django.contrib import messages
