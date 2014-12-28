054d40e585e9608b59686b1b38c16d45b2717528
lhark <lhark@hotmail.fr>
2014-12-28 18:42:13 +0100

    reverted to previous fix for the fetching of FTPs

--- home/templatetags/get_ftps.py
+++ home/templatetags/get_ftps.py
@@ -8,16 +8,6 @@ import urllib2
 register = template.Library()
 
 def fetchFTPs():
-<<<<<<< HEAD
-    response = urllib2.urlopen('http://porygon.rez', timeout=1).read()
-    soup = BeautifulSoup(response)
-    ftps = []
-    for ftp in soup.select('#ftp_list a'):
-        ftps.append({
-            'name': ftp.text,
-            'link': ftp.get('href')
-            })
-=======
     ftps = []
     try:
         response = urllib2.urlopen('http://porygon.rez', timeout=1).read()
@@ -27,9 +17,9 @@ def fetchFTPs():
                 'name': ftp.text,
                 'link': ftp.get('href')
                 })
-    except urllib2.URLError:
+    except urllib2.URLError:	#Porygon not available
         pass
->>>>>>> ff11b318f416cd17afff37561e23ab5487cadb52
+        
     return ftps
 
 @register.inclusion_tag('home/ftps_list.html')
@@ -37,20 +27,7 @@ def get_ftps():
     cache = get_cache('default')
     ftps = cache.get(settings.KEY_CACHE_FTPS)
     if ftps is None:
-<<<<<<< HEAD
-		try:
-			ftps = fetchFTPs()
-		except : #URLError:
-			#in case of porygon being unreachable
-			ftps = [{
-				'name': "Porygon unreachable",
-				'link': "htpp://paiji.rez"
-				}]
-        #Cache ftps info for 25 min
-		cache.set(settings.KEY_CACHE_FTPS, ftps, 60*25)
-=======
         ftps = fetchFTPs()
         # Cache ftps info for 25 min
         cache.set(settings.KEY_CACHE_FTPS, ftps, 60*25)
->>>>>>> ff11b318f416cd17afff37561e23ab5487cadb52
     return {'ftps': ftps}
