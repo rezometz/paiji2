e82f5d2f9c7535889cf231b112bb04502b5b615b
liusiqi43 <me@siqi.fr>
2015-01-10 00:21:14 +0100

    instant publish for shoutbox

--- bulletin_board/templatetags/bulletin.py
+++ bulletin_board/templatetags/bulletin.py
@@ -1,17 +1,18 @@
 from django import template
 
 from ..models import Note
+from ..forms import NoteForm
 
+from django.core.urlresolvers import reverse
 
 register = template.Library()
 
 
-@register.inclusion_tag(
-    'bulletin_board/bulletin_board_short.html',
-    takes_context=True,
-)
+@register.inclusion_tag('bulletin_board/bulletin_board_short.html', takes_context=True)
 def display_bulletin_board(context, nb=5):
-    return {
+	return {
         'request': context['request'],
         'notes': Note.objects.select_related('author').all()[:nb],
+        'form':NoteForm(),
+        'bulletin_add':reverse('bulletin-add'),
     }
