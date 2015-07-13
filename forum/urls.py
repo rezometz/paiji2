54a26f73abf09134a575a1514cae77bcc850b0f1
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-07-13 17:18:27 +0200

    login_required in all forum urls

--- forum/urls.py
+++ forum/urls.py
@@ -1,41 +1,42 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
 urlpatterns = [
     url(
         r'^$',
-        views.TopicListView.as_view(),
+        login_required(views.TopicListView.as_view()),
         name='topic-list'
     ),
     url(
         r'^page/(?P<page>[0-9]+)/$',
-        views.TopicListView.as_view(),
+        login_required(views.TopicListView.as_view()),
         name='topic-page'
     ),
     url(
         r'^recent/$',
-        views.NewMessagesView.as_view(),
+        login_required(views.NewMessagesView.as_view()),
         name='recent-list'
     ),
     url(
         r'^recent/page/(?P<page>[0-9]+)$',
-        views.NewMessagesView.as_view(),
+        login_required(views.NewMessagesView.as_view()),
         name='recent-page'
     ),
     url(
         '^message/(?P<pk>[0-9]+)$',
-        views.message_view,
+        login_required(views.message_view),
         name='message'
     ),
     url(
         '^message/(?P<pk>[0-9]+)/answer$',
-        views.AnswerCreate.as_view(),
+        login_required(views.AnswerCreate.as_view()),
         name='answer'
     ),
     url(
         '^new$',
-        views.AnswerCreate.as_view(),
+        login_required(views.AnswerCreate.as_view()),
         name='new'
     ),
 ]
