ce5cadb6ea9a8949470d9df6c184b78eade3c7e7
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-07-23 11:15:31 +0200

    i18n weather app

--- weather/templatetags/weather_tag.py
+++ weather/templatetags/weather_tag.py
@@ -2,6 +2,7 @@ from django import template
 from .weather_fetcher import WeatherFetcher
 from django.core.cache import get_cache
 from django.conf import settings
+from django.utils.translation import get_language
 
 
 register = template.Library()
@@ -12,10 +13,16 @@ def get_weather():
     cache = get_cache('default')
     weather = cache.get(settings.KEY_CACHE_WEATHER)
     if weather is None:
-        wf = WeatherFetcher('fr', 'json', 'Metz')
+        try:
+            wf = WeatherFetcher(get_language(), 'json', 'Metz')
+        except:
+            try:
+                wf = WeatherFetcher(settings.LANGUAGE_CODE, 'json', 'Metz')
+            except:
+                wf = WeatherFetcher('en', 'json', 'Metz')
         weather = wf.fetchWeather()
-        # Cache weather info for 25 min
-        cache.set(settings.KEY_CACHE_WEATHER, weather, 60*25)
+        # Cache weather info for 10 min
+        cache.set(settings.KEY_CACHE_WEATHER, weather, 60*10)
 
     return {
         'weather': weather,
