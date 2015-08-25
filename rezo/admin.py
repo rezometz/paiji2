ef2eea901f2addf181e0bc9f106c1d08127e9ab8
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-08-25 19:49:06 +0200

    a far better widget for the user's sidebars in the admin site

--- rezo/admin.py
+++ rezo/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
+from django import forms
+from modular_blocks.fields import ListTextField
 
 from .models import User
 
+class TextListInput(forms.TextInput):
+# widget for the admin editing
+# of sidebar_left and sidebar_right
+    def _format_value(self, value):
+        return ','.join(value)
 
 class UserAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        ListTextField: {
+            'widget': TextListInput,
+        },
+    }
     list_display = (
         'username',
         'first_name',
