64dcdc6e94ca3000f7a8d59a0e3e7196cdf83394
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-08-28 20:12:09 +0200

    improves confirmation page for switching language

--- paiji2/views.py
+++ paiji2/views.py
@@ -2,24 +2,24 @@ from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from django.views.i18n import set_language
 from django.conf import settings
+from django.utils.translation import get_language_info,\
+    check_for_language
 
 
 def confirm_language(request, code):
     if request.method == 'POST':
         return set_language(request)
 
-    languages = settings.LANGUAGES
-    new_language = None
-    for language_code, language_name in languages:
-        if language_code == code:
-            new_language = (language_code, language_name)
-
-    if new_language is None:
-        return redirect(reverse(settings.REDIRECT_URL))
+    try:
+        assert(check_for_language(code))
+        assert(code in dict(settings.LANGUAGES))
+        new_language = get_language_info(code)
+    except:
+        return redirect(settings.REDIRECT_URL)
 
     next_page = request.GET.get('next', settings.REDIRECT_URL)
 
     return render(request, 'paiji2/confirm_language.html', context={
-        'language': new_language,
+        'new_language': new_language,
         'next': next_page,
     })
